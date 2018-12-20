f524b84a61cd7036154a3d6c759519840a180dbb
Leo Liao <332488573@qq.com>
2018-12-20 19:06:50 +0800

    init

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+const router = require('koa-router')()
+const fs = require('fs');
+
+router.get('/', async (ctx, next) => {
+  ctx.response.type = 'html';
+  ctx.response.body = fs.createReadStream('./public/index.html');
+})
+
+router.get('/string', async (ctx, next) => {
+  ctx.body = 'koa2 string'
+})
+
+router.get('/json', async (ctx, next) => {
+  ctx.body = {
+    title: 'koa2 json'
+  }
+})
+
+module.exports = router
